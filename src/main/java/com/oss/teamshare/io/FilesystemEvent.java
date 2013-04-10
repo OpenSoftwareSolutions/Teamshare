2b228e12d7f2b84148bc2ef13d1d018ad360891e
Adriana Draghici <adriana.draghici@cs.pub.ro>
2013-04-10 17:40:19 +0300

    minor changes for filesystem events

--- src/main/java/com/oss/teamshare/io/FilesystemEvent.java
+++ src/main/java/com/oss/teamshare/io/FilesystemEvent.java
@@ -1,18 +1,34 @@
 package com.oss.teamshare.io;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.nio.file.Path;
-
-
+/**
+ * Event types that can occur for file or folders.
+ * @author adriana
+ *
+ */
+enum FileEventType{
+	CREATE, DELETE, MODIFY;
+}
+/**
+ * Represents an event related to a file or a folder inside a team's folder.
+ *
+ */
 public class FilesystemEvent {
-  
-  protected Path file;
 
-  public FilesystemEvent(Path file) {
-    super();
-    this.file = file;
-  }
+	protected TeamFile file;
+	protected FileEventType eventType;
 
-  public Path getFile() {
-    return file;
-  }
+	public FilesystemEvent(TeamFile file, FileEventType eventType) {
+		this.file = file;
+		this.eventType = eventType;
+	}
+
+	public TeamFile getFile() {
+		return file;
+	}
+
+	public FileEventType getEventType() {
+		return eventType;
+	}
 }
